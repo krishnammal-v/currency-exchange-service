2136668cc6782560529a1a68e1956578b2cc677e
krishnammal <krishnammal.r@yahoo.com>
2020-07-14 00:41:00 +0530

    Eureka changes

--- src/main/java/com/sample/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ src/main/java/com/sample/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -4,8 +4,10 @@ import java.math.BigDecimal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CurrencyExchangeServiceApplication {
 	
 	
